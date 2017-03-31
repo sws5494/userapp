80c296c627ea043ad36e1916585743bba09f2c4a
school <sws5494@naver.com>
2017-03-31 17:14:44 +0900

    UI 수정

--- app/src/main/java/com/example/user/userapp/MainActivity.java
+++ app/src/main/java/com/example/user/userapp/MainActivity.java
@@ -16,13 +16,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    TextView start_day, start_time, end_day, end_time;
-    Button btn_work, btn_eat, btn_etc;
+    TextView tv_start_day, tv_start_time, tv_end_day, tv_end_time;
+    Button btn_work, btn_eat, btn_etc, btn_request;
 
     DatePickerDialog date_dialog;
     TimePickerDialog time_dialog;
 
-    int month, day, hour, minute;
+    int start_year, start_month, start_day, start_hour, start_minute;
+    int end_year, end_month, end_day, end_hour, end_minute;
+    int reason;
+
     boolean Flag_day, Flag_time;
 
     @Override
@@ -30,14 +33,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start_day = (TextView) findViewById(R.id.start_day);
-        start_time = (TextView) findViewById(R.id.start_time);
-        end_day = (TextView) findViewById(R.id.end_day);
-        end_time = (TextView) findViewById(R.id.end_time);
+        tv_start_day = (TextView) findViewById(R.id.start_day);
+        tv_start_time = (TextView) findViewById(R.id.start_time);
+        tv_end_day = (TextView) findViewById(R.id.end_day);
+        tv_end_time = (TextView) findViewById(R.id.end_time);
 
         btn_work = (Button) findViewById(R.id.btn_work);
         btn_eat = (Button) findViewById(R.id.btn_eat);
         btn_etc = (Button) findViewById(R.id.btn_etc);
+        btn_request = (Button) findViewById(R.id.btn_request);
 
         long now = System.currentTimeMillis();
         Date date = new Date(now);
@@ -59,15 +63,15 @@ public class MainActivity extends AppCompatActivity {
         int strCurHour = Integer.parseInt(CurHourFormat.format(date));
         int strCurMinute = Integer.parseInt(CurMinuteFormat.format(date));
 
-        start_day.setText(strCurDate);
-        start_time.setText(strCurTime);
-        end_day.setText(strCurDate);
-        end_time.setText(strCurTime);
+        tv_start_day.setText(strCurDate);
+        tv_start_time.setText(strCurTime);
+        tv_end_day.setText(strCurDate);
+        tv_end_time.setText(strCurTime);
 
         date_dialog = new DatePickerDialog(this, date_listener, strCurYear, strCurMonth, strCurDay);
         time_dialog = new TimePickerDialog(this, time_listener, strCurHour, strCurMinute, false);
 
-        start_day.setOnClickListener(new View.OnClickListener() {
+        tv_start_day.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Flag_day = true;
@@ -75,21 +79,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        start_time.setOnClickListener(new View.OnClickListener() {
+
+        tv_start_time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Flag_time = true;
                 time_dialog.show();
             }
         });
-        end_day.setOnClickListener(new View.OnClickListener() {
+
+        tv_end_day.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Flag_day = false;
                 date_dialog.show();
             }
         });
-        end_time.setOnClickListener(new View.OnClickListener() {
+
+        tv_end_time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Flag_time = false;
@@ -100,58 +107,80 @@ public class MainActivity extends AppCompatActivity {
         btn_work.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                reason = 1;
                 btn_eat.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_etc.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_work.setBackgroundColor(Color.rgb(217, 217, 217));
             }
         });
+
         btn_eat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                reason = 2;
                 btn_work.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_etc.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_eat.setBackgroundColor(Color.rgb(217, 217, 217));
             }
         });
+
         btn_etc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                reason = 3;
                 btn_work.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_eat.setBackgroundColor(Color.rgb(255, 255, 255));
                 btn_etc.setBackgroundColor(Color.rgb(217, 217, 217));
+            }
+        });
+
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "시작: " + start_year + "/" + start_month + "/" + start_day + "/" + start_hour + "/" + start_minute, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "종료: " + end_year + "/" + end_month + "/" + end_day + "/" + end_hour + "/" + end_minute, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "사유: " + reason, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private DatePickerDialog.OnDateSetListener date_listener = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        public void onDateSet(DatePicker view, int yearOfYear, int monthOfYear, int dayOfMonth) {
             if (Flag_day == true) {
                 if (0 < monthOfYear && monthOfYear < 10) {
-                    start_day.setText(year + "년 " + "0" + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
+                    start_year = yearOfYear;
+                    start_month = monthOfYear + 1;
+                    start_day = dayOfMonth;
+                    tv_start_day.setText(yearOfYear + "년 " + "0" + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
                 } else {
-                    start_day.setText(year + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
+                    tv_start_day.setText(yearOfYear + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
                 }
             } else {
+                end_year = yearOfYear;
+                end_month = monthOfYear + 1;
+                end_day = dayOfMonth;
                 if (0 < monthOfYear && monthOfYear < 10) {
-                    end_day.setText(year + "년 " + "0" + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
+                    tv_end_day.setText(yearOfYear + "년 " + "0" + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
                 } else {
-                    end_day.setText(year + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
+                    tv_end_day.setText(yearOfYear + "년 " + (monthOfYear + 1) + "월 " + dayOfMonth + "일");
                 }
             }
-//            Toast.makeText(getApplicationContext(), year + "년" + (monthOfYear + 1) + "월" + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
         }
     };
 
     private TimePickerDialog.OnTimeSetListener time_listener = new TimePickerDialog.OnTimeSetListener() {
         @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfMinute) {
             if (Flag_time == true) {
-                start_time.setText(hourOfDay + "시 " + minute + "분");
+                start_hour = hourOfDay;
+                start_minute = minuteOfMinute;
+                tv_start_time.setText(hourOfDay + "시 " + minuteOfMinute + "분");
             } else {
-                end_time.setText(hourOfDay + "시 " + minute + "분");
+                end_hour = hourOfDay;
+                end_minute = minuteOfMinute;
+                tv_end_time.setText(hourOfDay + "시 " + minuteOfMinute + "분");
             }
-//            Toast.makeText(getApplicationContext(), hourOfDay + "시 " + minute + "분", Toast.LENGTH_SHORT).show();
         }
     };
 }
